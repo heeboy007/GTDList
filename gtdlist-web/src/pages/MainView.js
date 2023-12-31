90db883a0d5b2b54260b79c9017a9120e9d97411
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2024-01-01 03:02:40 +0900

    add: logo to MainPage, will do the other things later

--- gtdlist-web/src/pages/MainView.js
+++ gtdlist-web/src/pages/MainView.js
@@ -1,10 +1,35 @@
+/** @jsxImportSource @emotion/react */
 import { NavLink } from "react-router-dom";
+import { css } from "@emotion/react";
+
+import logo from './temp_samples/FlowForge-Longer.svg';
 
 function MainView() {
     return (
-        <div className="mainView">
-            hello this is main page!
-            <NavLink to='/listview'>Main List</NavLink>
+        <div 
+            className="mainView"
+            css={css`
+                width: 960px;
+                height: 100%;
+                max-width: 960px;
+                display: flex;
+                flex-direction: column;
+                padding: 0;
+                margin: 0 auto;
+                box-sizing: content-box;
+                background: white;
+                overflow-y: auto;
+            `}>
+            <div>
+                <span><img src={logo} alt="logo"/></span>
+                <nav>
+                    <NavLink to='/listview'>Main List</NavLink>
+                </nav>
+                <div>
+                    <button>로그인</button>
+                    <button>회원가입</button>
+                </div>
+            </div>
         </div>
     );
 }

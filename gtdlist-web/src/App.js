c88cea48ab701284a3f4ed8b5c455c8564839397
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2024-01-01 15:49:25 +0900

    add: basic main view page

--- gtdlist-web/src/App.js
+++ gtdlist-web/src/App.js
@@ -13,6 +13,12 @@ function App() {
                 width: 100vw;
                 height: 100vh;
                 background-color: #eee;
+                @font-face {
+                    font-family: 'S-CoreDream-3Light';
+                    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
+                    font-weight: normal;
+                    font-style: normal;
+                }
             `}>
             <Routes>
                 <Route path='/' Component={MainView}/>
--- gtdlist-web/src/pages/MainView.js
+++ gtdlist-web/src/pages/MainView.js
@@ -1,8 +1,8 @@
 /** @jsxImportSource @emotion/react */
-import { NavLink } from "react-router-dom";
 import { css } from "@emotion/react";
 
-import logo from './temp_samples/FlowForge-Longer.svg';
+import NaviGationBar from "./MainComponents/NavigationBar";
+import Impression from "./MainComponents/Impression";
 
 function MainView() {
     return (
@@ -20,16 +20,8 @@ function MainView() {
                 background: white;
                 overflow-y: auto;
             `}>
-            <div>
-                <span><img src={logo} alt="logo"/></span>
-                <nav>
-                    <NavLink to='/listview'>Main List</NavLink>
-                </nav>
-                <div>
-                    <button>로그인</button>
-                    <button>회원가입</button>
-                </div>
-            </div>
+            <NaviGationBar />
+            <Impression />
         </div>
     );
 }
